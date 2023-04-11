9789251301b5ad06ce0e6519d90a5d10a9d65f79
Nikolaus Satria <nikolaussatria@gmail.com>
2023-04-11 16:45:57 +0700

    Revert "class"
    This reverts commit e81509cc031d8024c2b5cc97ecfd14de13a1838a.

--- rawatJalan_LIB/Posisi.cs
+++ rawatJalan_LIB/Posisi.cs
@@ -8,19 +8,17 @@ namespace rawatJalan_LIB
 {
     public class Posisi
     {
-        int id;
-        string nama;
+        private int id;
+        private string nama;
 
         public Posisi(int id, string nama)
         {
             Id = id;
             Nama = nama;
         }
-
-        public Posisi()
+        public Posisi(string nama)
         {
-            Id = 0;
-            Nama = "";
+            Nama = nama;
         }
 
         public int Id { get => id; set => id = value; }
